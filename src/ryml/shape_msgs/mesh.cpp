ad375151605063091eef1b55fa9a8958665a07be
Captain Yoshi <captain.yoshisaur@gmail.com>
2022-12-04 16:06:16 -0500

    [impl] add alternate Mesh decoding as a resource
    Can specify mesh as a resource, e.g. `package://pkg/mesh.stl`

--- src/ryml/shape_msgs/mesh.cpp
+++ src/ryml/shape_msgs/mesh.cpp
@@ -1,3 +1,7 @@
+#include <Eigen/Core>
+
+#include <geometric_shapes/shape_operations.h>
+
 #include <moveit_serialization/ryml/std/std.h>
 #include <moveit_serialization/ryml/geometry_msgs/point.h>
 #include <moveit_serialization/ryml/shape_msgs/mesh_triangle.h>
@@ -17,11 +21,42 @@ void write(c4::yml::NodeRef* n, shape_msgs::Mesh const& rhs)
 
 bool read(c4::yml::ConstNodeRef const& n, shape_msgs::Mesh* rhs)
 {
-    if (n.has_child("triangles"))
-        n["triangles"] >> rhs->triangles;
-    if (n.has_child("vertices"))
-        n["vertices"] >> rhs->vertices;
+    // optional decoding as a resource URI, e.g. package://my_pkg_name/mesh.stl
+    if (n.has_child("resource")) {
+        std::string resource;
+        n["resource"] >> resource;
 
+        shapes::ShapeMsg shape_msg;
+        Eigen::Vector3d scaling{ 1, 1, 1 };
+
+        if (n.has_child("dimensions")) {
+            std::vector<double> dimensions;
+            n["dimensions"] >> dimensions;
+
+            if (dimensions.size() != 3)
+                return false;
+            scaling.x() = dimensions[0];
+            scaling.y() = dimensions[1];
+            scaling.z() = dimensions[2];
+        }
+
+        std::unique_ptr<const shapes::Shape> shape =
+            std::unique_ptr<const shapes::Shape>(shapes::createMeshFromResource(resource, scaling));
+        if (!shape)
+            return false;
+
+        shapes::constructMsgFromShape(shape.get(), shape_msg);
+
+        auto& mesh = boost::get<shape_msgs::Mesh>(shape_msg);
+        rhs->triangles = mesh.triangles;
+        rhs->vertices = mesh.vertices;
+
+    } else {
+        if (n.has_child("triangles"))
+            n["triangles"] >> rhs->triangles;
+        if (n.has_child("vertices"))
+            n["vertices"] >> rhs->vertices;
+    }
     return true;
 }
 
